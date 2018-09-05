0990beddae3fc41c02f21a5cbafff58a8eb2cf7a
lbarret4 <llewellynbar4@gmail.com>
2018-09-04 23:56:32 -0400

    setup chirp class

---
+++ chirper-app/src/components/Chirps.jsx
@@ -0,0 +1,23 @@
+import React, { Component,Fragment } from 'react';
+
+
+class Chirps extends Component{
+
+    // constructor(props){
+        
+    //     this.state={
+
+    //     }
+    // }
+    render(){
+        return(
+            <Fragment>
+             
+                
+            </Fragment>
+        );
+    }
+
+
+}
+export default Chirps;
