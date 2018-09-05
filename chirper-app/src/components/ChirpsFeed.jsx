797ae90f1e42d316a377fe5125d24e73634f268b
lbarret4 <llewellynbar4@gmail.com>
2018-09-05 14:21:49 -0400

    Relocated input and button here and added corresponding event handlers

--- chirper-app/src/components/ChirpsFeed.jsx
+++ chirper-app/src/components/ChirpsFeed.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Chirps from './Chirps'
+import ChirpList from './ChirpList';
 
 
 
@@ -7,19 +8,57 @@ class ChirpsFeed extends Component {
 
     constructor(props){
         super(props);
+        this.state= {
+            chirpList:[],
+            chirpPost:" ",
+        }
+        this.handlesNewChirp = this.handlesNewChirp.bind(this);
+        this.handlesPost = this.handlesPost.bind(this);
+    }
+
+    componentDidMount(){
+        this.setState({
+                 chirpList:[
+                'Cras justo odio',
+                'Dapibus ac facilisis in',
+                'Vestibulum at eros',
+            ]
+        })
+    }
+
+    handlesNewChirp(e){
+        this.setState({
+            chirpPost:e.target.value
+        });
 
     }
 
+    handlesPost(e){
+        e.preventDefault();
+        let oddList = this.state.chirpList;
+        oddList.unshift(this.state.chirpPost);
+        this.setState({
+            chirpList:oddList,
+            chirpPost:""
+        })
 
+    }
 
     render() {
+        
         return (
             <div className="card mt-5" style={{ maxWidth: " 90vw" }} >
                 <div className="card-header">
                     To get started, type a message and post a chirp.
                 </div>
-
-                <Chirps items={this.props.items} />
+                <div className="input-group">
+                    <textarea className="form-control"onChange={this.handlesNewChirp} value={this.state.chirpPost} / >
+                        
+                    <div className="input-group-append">
+                    <button type="button" className=" btn btn-primary" onClick= {this.handlesPost} >Post</button>
+                    </div>
+                </div>
+                <ChirpList items={this.state.chirpList} />
 
 
 
