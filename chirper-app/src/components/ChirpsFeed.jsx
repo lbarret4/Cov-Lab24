e45bb9076e873bef300436519070cfb5b7cf26e8
lbarret4 <llewellynbar4@gmail.com>
2018-09-05 00:57:31 -0400

    change anchor to button and fixed text area sizing due to typo

--- chirper-app/src/components/ChirpsFeed.jsx
+++ chirper-app/src/components/ChirpsFeed.jsx
@@ -7,14 +7,20 @@ class ChirpsFeed extends Component {
 
     render() {
         return (
-            <div className="card mt-5" style={{maxWidth:" 90vw"}} >
+            <div className="card mt-5" style={{ maxWidth: " 90vw" }} >
                 <div className="card-header">
                     To get started, type a message and post a chirp.
                 </div>
-                <textarea className="card-img-top" ></textarea>
-                <div className="card-body text-center">
-                    <a href="" className="btn btn-primary">Post</a>
+                <div className="input-group">
+                    <textarea className="form-control" ></textarea>
+                    <div className="input-group-append">
+                    <button type="button" className=" btn btn-primary">Post</button>
+                    </div>
                 </div>
+
+                {/* <div className="card-body text-center">
+                    <a href="" className="btn btn-primary">Post</a>
+                </div> */}
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item">Cras justo odio</li>
                     <li className="list-group-item">Dapibus ac facilisis in</li>
